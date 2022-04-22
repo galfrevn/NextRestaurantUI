be8dad8d0571bdbff74895e0fd6b8772460ee641
ValentinGalfre <valentin.galfre@4rsoluciones.com>
2022-04-22 14:37:26 -0300

    Font fix?

--- pages/_document.js
+++ pages/_document.js
@@ -10,13 +10,13 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;600;700&display=swap"
+            rel="stylesheet"
+          />
           <link rel="shortcut icon" href="/favicon.svg" />
           <link rel="manifest" href="/manifest.json" />
           <meta name="theme-color" content="#fecb54" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
-            rel="stylesheet"
-          />
         </Head>
         <body>
           <Main />
